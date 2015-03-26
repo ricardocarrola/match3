ae3ccc37ace29daee58bdaff3d919257d0a5c7d8
ricardo.carrola@gmail.com <ricardo.carrola@gmail.com>
2015-03-26 10:13:17 +0000

    Special powers release

--- js/game.js
+++ js/game.js
@@ -1,4 +1,14 @@
-
+	function speak(what){
+		try{
+							var u = new SpeechSynthesisUtterance();
+							
+							 u.text = what
+							 u.lang = 'en-US';
+							 u.rate = 1;
+							 u.onend = function(event) {  }
+							 speechSynthesis.speak(u); 
+			}catch(e){}
+	}
 	function Level(Settings){
 			var level={};
 			for(var i=0;i<Settings.levels.length;i++){
@@ -8,14 +18,7 @@
 			}
 			setTimeout(function(){
 				$(document).trigger("newScore",[level.name]);				
-				try{
-							var u = new SpeechSynthesisUtterance();
-							 u.text = level.name
-							 u.lang = 'en-US';
-							 u.rate = 1;
-							 u.onend = function(event) {  }
-							 speechSynthesis.speak(u); 
-			}catch(e){}
+			
 			},1000)
 			
 			$("body").css("background","url('"+(level.background ? level.background : 'img/back_1.png')+"')");
@@ -24,6 +27,48 @@
 	function Board(Config,Settings,LevelSettings){
 		this.level = new Level(LevelSettings);
 		//two dimensional board
+		this.what =[];
+		this.clearLine=function(line){
+			for(var column=0;column<this.width;column++){
+				this.boardPieces[line][column].piece={};
+				console.log("exploding %d,%d",line,column);
+				removePiece(this.boardPieces[line][column]);
+				
+				//$(document).trigger("removePiece",[]);
+			}
+		}
+		this.bombAll=function(){
+			var type= this.boardPieces[this.what[0][0]][this.what[0][1]].piece.color!='-' ? this.boardPieces[this.what[0][0]][this.what[0][1]].piece.color : this.boardPieces[this.what[1][0]][this.what[1][1]].piece.color;
+			this.boardPieces[this.what[0][0]][this.what[0][1]].piece =  {};	
+			this.boardPieces[this.what[1][0]][this.what[1][1]].piece =  {};	
+			removePiece(this.boardPieces[this.what[0][0]][this.what[0][1]]);	
+			removePiece(this.boardPieces[this.what[1][0]][this.what[1][1]]);	
+			for(var i=0;i<this.width;i++){
+				for(var j=0;j<this.height;j++){
+						if (this.boardPieces[i][j].piece.color==type){
+							this.boardPieces[i][j].piece =  {};	
+							removePiece(this.boardPieces[i][j]);									
+						}																											
+				}
+			}
+			setTimeout(function(){
+					for(var i=0;i<b.width;i++)
+						for(var j=0;j<b.height;j++)
+								if (!b.boardPieces[i][j].piece.color){
+									b.boardPieces[i][j].piece =  new Piece(config.pieces);	
+									addPiece(b.boardPieces[i][j]);	
+								}									
+						},100);			
+		}
+		this.clearColumn=function(column){
+			for(var line=0;line<this.height;line++){
+				this.boardPieces[line][column].piece={};
+				
+				removePiece(this.boardPieces[line][column]);
+				
+				//$(document).trigger("removePiece",[]);
+			}
+		}
 		this.boardPieces= [[]];
 		this.width			 	 = (!Config.size) ?  '7' : Config.size;
 		this.height			 = (!Config.height) ?  '7' : Config.height;
@@ -49,6 +94,15 @@
 		this.getBoard=function(){
 				return this.boardPieces;
 		}
+		this.specialPowersInvoke=function(piece,i,j){
+			
+			if (piece.powers){
+				piece.powers(i,j);
+				document.getElementById("tasty").play();	
+			}
+				
+			
+		}
 		this.swapPieces=function(p1,p2){
 			var move=false;
 			if (p1[0]==p2[0] && ( p1[1]+1==p2[1]  || p1[1]-1==p2[1] || p2[1]+1==p1[1]  || p2[1]-1==p1[1])){
@@ -63,9 +117,29 @@
 						this.boardPieces[p2[0]][p2[1]].piece = tmp;												
 						$(document).trigger("transferPiece",[{"from":this.boardPieces[p1[0]][p1[1]],"to":this.boardPieces[p2[0]][p2[1]]}]);
 						this.checkPattern(p1,p2);
+						if (this.boardPieces[p1[0]][p1[1]].piece.color=="-")
+							if (this.boardPieces[p1[0]][p1[1]].piece.powers)
+								this.boardPieces[p1[0]][p1[1]].piece.powers();
+						if (this.boardPieces[p2[0]][p2[1]].piece.color=="-")
+							if (this.boardPieces[p2[0]][p2[1]].piece.powers)
+								this.boardPieces[p2[0]][p2[1]].piece.powers();
 						return move;
 			}								
 			return move;
+		}
+		this.shuffle=function(){
+			for(var i=0;i<this.width;i++){
+				for(var j=0;j<this.height;j++){
+						this.boardPieces[i][j].piece =  new Piece(config.pieces);
+						closePiece(this.boardPieces[i][j]);																									
+				}
+			}
+			setTimeout(function(){
+				for(var i=0;i<b.width;i++)
+				for(var j=0;j<b.height;j++)
+					addPiece(b.boardPieces[i][j]);																				
+			},100);
+			$(document).trigger("newScore",["Shuffleling"]);
 		}
 		
 		this.checkPattern=function(p1,p2){			
@@ -83,11 +157,17 @@
 										if (this.boardPieces[i][j].piece.color!=was){
 											
 											if (clean>=3){ //need to clean?
-													for(var k=0;k<what.length;k++){															
-															this.boardPieces[what[k].i][what[k].j]={"disabled":false,"piece":{},"i":what[k].i,"j":what[k].j,"id":what[k].i+"_"+what[k].j};
-															$(document).trigger("removePiece",[what[k]]);
+													for(var k=0;k<what.length;k++){						
+															//removed all the pieces but if a specified pattern is met we need special pieces to be put here!
+															this.specialPowersInvoke(this.boardPieces[what[k].i][what[k].j].piece,what[k].i,what[k].j);
+															this.boardPieces[what[k].i][what[k].j]={"disabled":false,"piece":{},"i":what[k].i,"j":what[k].j,"id":what[k].i+"_"+what[k].j};																														
+															removePiece(what[k]);
 															howMany.push(clean);
 															perType.push(was);
+													}
+													if (clean==4){														
+														this.boardPieces[what[3].i][what[3].j]={"disabled":false,"piece":new Piece(config.pieces,was),"i":what[3].i,"j":what[3].j,"id":what[3].i+"_"+what[3].j};														
+														$(document).trigger("addPiece",[this.boardPieces[what[3].i][what[3].j]]);
 													}
 													
 											}
@@ -103,12 +183,18 @@
 																										
 								}
 								if (clean>=3){ //need to clean?
-										for(var k=0;k<what.length;k++){															
+										for(var k=0;k<what.length;k++){					
+												this.specialPowersInvoke(this.boardPieces[what[k].i][what[k].j].piece,what[k].i,what[k].j);										
 												this.boardPieces[what[k].i][what[k].j]={"disabled":false,"piece":{},"i":what[k].i,"j":what[k].j,"id":what[k].i+"_"+what[k].j};
-												$(document).trigger("removePiece",[what[k]]);
+												removePiece(what[k]);
 												howMany.push(clean);
 												perType.push(was);
 										}
+										if (clean==4){
+														
+														this.boardPieces[what[3].i][what[3].j]={"disabled":false,"piece":new Piece(config.pieces,was),"i":what[3].i,"j":what[3].j,"id":what[3].i+"_"+what[3].j};
+														$(document).trigger("addPiece",[this.boardPieces[what[3].i][what[3].j]]);
+													}
 										what = [];
 								}								
 							}								
@@ -124,14 +210,20 @@
 											if (this.boardPieces[j][i].piece.color!=was){
 											
 											if (clean>=3){ //need to clean?
-													for(var k=0;k<what.length;k++){															
+													for(var k=0;k<what.length;k++){	
+															this.specialPowersInvoke(this.boardPieces[what[k].i][what[k].j].piece,what[k].i,what[k].j);																							
 															this.boardPieces[what[k].i][what[k].j]={"disabled":false,"piece":{},"i":what[k].i,"j":what[k].j,"id":what[k].i+"_"+what[k].j};
-															$(document).trigger("removePiece",[what[k]]);
+															removePiece(what[k]);
 															howMany.push(clean);
 															perType.push(was);
 													}
 													
 											}
+											if (clean==4){
+													
+														this.boardPieces[what[3].i][what[3].j]={"disabled":false,"piece":new Piece(config.pieces,was),"i":what[3].i,"j":what[3].j,"id":what[3].i+"_"+what[3].j};
+														$(document).trigger("addPiece",[this.boardPieces[what[3].i][what[3].j]]);
+													}
 											was= this.boardPieces[j][i].piece.color;
 											what = [];
 											what.push(this.boardPieces[j][i]);
@@ -144,17 +236,28 @@
 																						
 								}
 								if (clean>=3){ //need to clean?
-										for(var k=0;k<what.length;k++){															
+										for(var k=0;k<what.length;k++){			
+												this.specialPowersInvoke(this.boardPieces[what[k].i][what[k].j].piece,what[k].i,what[k].j);																				
 												this.boardPieces[what[k].i][what[k].j]={"disabled":false,"piece":{},"i":what[k].i,"j":what[k].j,"id":what[k].i+"_"+what[k].j};
-												$(document).trigger("removePiece",[what[k]]);
+												removePiece(what[k]);
 												howMany.push(clean);
 												perType.push(was);
 										}
+										if (clean==4){
+													
+														this.boardPieces[what[3].i][what[3].j]={"disabled":false,"piece":new Piece(config.pieces,was),"i":what[3].i,"j":what[3].j,"id":what[3].i+"_"+what[3].j};
+														$(document).trigger("addPiece",[this.boardPieces[what[3].i][what[3].j]]);
+													}
 										what = [];
 								}								
 							}															
 			//step 2
-			
+			var t = this;
+			setTimeout(function(){
+				t.checkPattern2(howMany,perType);
+			},20);
+		}
+		this.checkPattern2=function(howMany,perType){
 			this.doScore(howMany,perType);
 			
 			
@@ -243,15 +346,33 @@
 	}
 	
 	function Settings(Config){
-		this.pieces = Config.pieces;
+		this.pieces    = Config.pieces;	
+	
 	}
 	
-	function Piece(Config){
+	function Piece(Config,type){
 		var types = Config;
+		//random but with the right type
 		var randomType = Math.floor((Math.random() * types.length) + 0);
-		//T1 if no configuration just give a sample one
-		this.color   = types[randomType].color;
-		this.image =  types[randomType].image;
+		var randomType2 = "";
+		if (type){
+			for(var i=0;i<types.length;i++)
+				if (types[i].color==type)
+						randomType = i;
+		}
+		if (type && types[randomType].specials){
+			randomType2= Math.floor((Math.random() * types[randomType].specials.length) + 0);	
+			this.color   =  types[randomType].specials[randomType2].color;
+			this.image =  types[randomType].specials[randomType2].image;	
+			this.powers =  types[randomType].specials[randomType2].powers;
+		}
+		else{
+			this.color   = types[randomType].color;
+			this.image =  types[randomType].image;	
+			this.powers = types[randomType].powers;
+		}
+		
+		
 		
 	}
 	
